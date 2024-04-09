58907ba37d09cdbac66fb790094d0852580a0492
ShiftyX1 <143071695+ShiftyX1@users.noreply.github.com>
2024-04-10 01:01:25 +0300

    changed db to postgresql

--- core/config.py
+++ core/config.py
@@ -1,13 +1,21 @@
 import os
 from dotenv import load_dotenv
 
+from pathlib import Path
 
-load_dotenv()
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 
 class Settings:
-    SQL_DATABASE_URL: str = "sqlite:///test.db"
-
+    DB_HOST: str = os.getenv('DB_HOST')
+    DB_PORT: str = os.getenv("DB_PORT")
+    DB_USER: str = os.getenv("DB_USER")
+    DB_NAME: str = os.getenv("DB_NAME")
+    DB_PASS: str = os.getenv("DB_PASS")
+    SQL_DATABASE_URL: str = (f"postgresql://{DB_USER}:"
+                             f"{DB_PASS}@{DB_HOST}:"
+                             f"{DB_PORT}/{DB_NAME}")
 
     print(f"Загруженные настройки проекта:\n1) База данных - {SQL_DATABASE_URL.split(":")[0]}\n2) URL базы данных - {SQL_DATABASE_URL}")
 
--- migrations/env.py
+++ migrations/env.py
@@ -5,10 +5,18 @@ from sqlalchemy import pool
 
 from alembic import context
 from db.models import metadata
+from core.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+section = config.config_ini_section
+config.set_section_option(section=section, name="DB_HOST", value=settings.DB_HOST)
+config.set_section_option(section=section, name="DB_PORT", value=settings.DB_PORT)
+config.set_section_option(section=section, name="DB_USER", value=settings.DB_USER)
+config.set_section_option(section=section, name="DB_PASS", value=settings.DB_PASS)
+config.set_section_option(section=section, name="DB_NAME", value=settings.DB_NAME)
 
 
 # Interpret the config file for Python logging.
