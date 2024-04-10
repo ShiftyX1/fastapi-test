c1609359f2bb58b97756b61d0cae0ad15127df39
ShiftyX1 <143071695+ShiftyX1@users.noreply.github.com>
2024-04-11 00:40:49 +0300

    commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from typing import List
+from pydantic import BaseModel, Field
+
 from db import base
 
 
@@ -13,3 +16,38 @@ def root(request: Request):
 
 
 
+fake_devices = [
+    {"id": 1234, "serial_number": 12345, "name": "Lenovo"},
+    {"id": 1221, "serial_number": 12211, "name": "Asus"},
+]
+
+
+class Rank(BaseModel):
+    id: int
+    name: str
+    level_privileges: int = Field(gt=0, le=12)
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    first_name: str
+    last_name: str
+    rank: List[Rank]
+
+
+class Device(BaseModel):
+    id: int
+    serial_number: int = Field(ge=0)
+    name: str = Field(max_length=10)
+
+
+@app.post("/device")
+def add_device(devices: List[Device]):
+    fake_devices.extend(devices)
+    return {"status": 200, "data": fake_devices}
+
+
+@app.get("/list_device/{device_id}", response_model=List[Device])
+def get_device(device_id: int):
+    return [device for device in fake_devices if device.get("id") == device_id]
